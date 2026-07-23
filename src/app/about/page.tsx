5929827eb6993630706140d8ec389330ea59ec1f
Aaron Miyagishima <aaron@okanaganlighting.ca>
2026-07-23 08:33:58 -0700

    Update contact email

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -258,8 +258,8 @@ function ContactSection() {
         <div className="space-y-5 border-t border-white/10 pt-8 text-base text-white/80 lg:border-l lg:border-t-0 lg:pl-12 lg:pt-0 md:text-lg">
           <ContactLine
             icon={Mail}
-            text="hello@giantsheadelectric.ca"
-            href="mailto:hello@giantsheadelectric.ca"
+            text="aaron@giantsheadelectric.ca"
+            href="mailto:aaron@giantsheadelectric.ca"
           />
 
           <ContactLine
